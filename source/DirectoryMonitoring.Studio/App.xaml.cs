cd8987e8fc036675d89e062ceb299f40496ec568
cvosoftware <cvosoftware@gmail.com>
2020-04-26 20:14:38 +0300

    (feature): Initial project

---
+++ source/DirectoryMonitoring.Studio/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using DirectoryMonitoring.Studio.Base;
+using DirectoryMonitoring.Studio.ViewModel;
+using DirectoryMonitoring.Studio.View;
+
+namespace Studio
+{
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var viewModel = new MainViewModel();
+            var view = new MainWindow();
+
+            view.DataContext = viewModel;
+            view.Show();
+
+        }
+    }
+}
---
+++ source/DirectoryMonitoring.Studio/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+[assembly: AssemblyTitle("Studio")]
+[assembly: AssemblyCompany("CVOSoftware")]
+[assembly: AssemblyProduct("DirectoryMonitoring")]
+[assembly: AssemblyCopyright("CVOSoftware ©  2020")]
+[assembly: ComVisible(false)]
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None,
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
